56f900a6b6c8200d8452653e5a4fe66a5b2dec7c
stanoja <bobanstanoja@hotmail.com>
2016-05-23 23:53:10 +0200

    renamed to concent.php

--- include/content.php
+++ include/content.php
@@ -7,11 +7,19 @@ class DBContent{
 
     //public $blog
     private static $sqlstr = 'select t1.blHeader, t1.blText, t1.blDateAdded, t2.usName, t2.usSurname from tblBlog as t1 left join tblUsers as t2 on t1.blUser = t2.usEmail order by t1.blDateAdded DESC;';
+    private static $sqlstr2 = '';
 
     public static function GetBlog() {
         global $database;
         
         $tmpblog = $database->table(self::$sqlstr);
         return $tmpblog;
+    }
+
+    public static function GetProductsByUser($useremail){
+        global $database;
+
+        $tmpresult = $database->table(self::$sqlstr2);
+        return $tmpresult;
     }
 }
