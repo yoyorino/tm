947fe5aea4969872534a1354a64bf1144b940d7f
stanoja <bobanstanoja@hotmail.com>
2016-05-31 10:44:01 +0200

    Ponedelnik no 4

--- include/filterwerk.php
+++ include/filterwerk.php
@@ -8,71 +8,48 @@
  */
 class filterwerk
 {
-    private $words = array(
-        array("great", 5),
-        array("bad", -3),
-        array("awful", -5),
-        array("dying", -5),
-        array("very lost", -3),
-        array("love", 5),
-        array("unhappy", -3),
-        array("very bad", -3),
-        array("big problem", -5),
-        array("slow", -3),
-        array("fast", 3),
-        array("reliable", 3),
-        array("incredible", 5),
-        array("crap", -5),
-        array("best", 10),
-        array("overheat", -1),
-        array("rubbish", -5)
+    private $words;
+    private $productid;
+    private $dbrez;
 
-    );
-
-    public function _constructor(){
-        
+    public function __construct($prid){
+        $this->productid=$prid;
+        $this->dbrez = new DBResult();
     }
 
-    public function CalculatePoints($commstr){
+    public function initKeyWords(){
+        $dbkw = new DBKeywords();
+        $tablekw = $dbkw->readContents($this->productid);
+        if (count($tablekw) > 0){
+            $this->words = $tablekw;
+        }
+        //$tmpkw = DBBlog::readContents();
+        echo 'keywords: <b>';
+        var_dump($tablekw);
+        echo '</b> <br />';
+    }
+
+    public function CalculatePoints($commstr, $comId, $date){
         $result = array();
+        //echo 'commstr:' . $commstr;
+        //var_dump($commstr);
         $tmpscore = 0;
         $maxscore = 0;
         $pscore = 0;
         $nscore = 0;
-        for ($i = 0; $i< count($this->words);$i++) {
-            if (strpos($commstr, $this->words[$i][0]) !== false) {
-                $tmpscore += $this->words[$i][1];
-                if ($this->words[$i][1] > 0)
-                    $pscore += $this->words[$i][1];
-                else
-                    $nscore += $this->words[$i][1];
+
+        $str = (string)$commstr;
+        echo '<br /><p>';
+        var_dump($str);
+        echo '</p><br />';
+        echo '<br />Pronajdeni zborovi: ';
+
+        foreach ($this->words as $kz){
+            if (strpos($str, $kz->kzIzraz) !== false){
+                echo $kz->kzIzraz . ' ';
+                $this->dbrez->insertContents($comId, $kz->kzId , $date);
             }
         }
-        $add = abs($nscore);
-        $maxscore = $pscore + $add;
-        $tmpscore += $add;
-        array_push($result, $tmpscore);
-        array_push($result, $maxscore);
-        $percent = ($tmpscore/$maxscore)*100;
-        $bod = "";
-        //printf("%d / %d, %d%%<br />", $tmpscore, $maxscore, $percent);
-        if ($percent < 40 )
-            $bod = "Negative";
-        else if ($percent > 60)
-            $bod = "Positive";
-        else
-            $bod = "Neutral";
-
-        if (($tmpscore == 0) && ($maxscore == 0)) {
-            $bod = "Neutral";
-            $percent=50;
-        }
-        //printf(" %s <br />", $bod);
-
-        
-        array_push($result, $percent);
-        array_push($result, $pscore);
-        array_push($result, $nscore);
         return $result;
     }
 }
